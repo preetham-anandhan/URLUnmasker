83a5fb693f18ee5cd408e0b901ef6fddbc99d20c
preetham_anandhan <preethamanand037@email.com>
2026-05-01 18:37:13 +0530

    updated api url

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on("messageCreate", async (message) => {
       message.reply("🔍 Fetching domain info...");
 
       const res = await axios.get(
-        `https://urlunmasker-api.onrender.com/api/domain/${domain}`
+        `https://urlunmasker.onrender.com/api/domain/${domain}`
       );
 
       const data = res.data;
